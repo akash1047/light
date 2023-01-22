af1aa5d0ac139e96c5d29c9cf1c7abee0957f184
akash1047 <94453657+akash1047@users.noreply.github.com>
2023-01-22 20:10:31 +0530

    draw via mouse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+use std::io::Write;
+use std::io::{stdin, stdout};
+
+use termion::cursor;
+use termion::{
+    cursor::Goto,
+    event::{Event, Key::*, MouseButton, MouseEvent},
+    input::{MouseTerminal, TermRead},
+    raw::IntoRawMode,
+    screen::IntoAlternateScreen,
+};
+
 fn main() {
-    println!("Hello, world!");
+    println!("press a key to start");
+
+    let mut screen = MouseTerminal::from(
+        stdout()
+            .into_raw_mode()
+            .and_then(IntoAlternateScreen::into_alternate_screen)
+            .unwrap(),
+    );
+
+    if let Err(_) = write!(screen, "{}", cursor::Hide) {
+        return;
+    }
+
+    for event in stdin().events() {
+        let event = match event {
+            Ok(e) => e,
+            Err(_) => {
+                break;
+            }
+        };
+
+        match event {
+            Event::Key(Char('q')) => {
+                break;
+            }
+
+            Event::Mouse(MouseEvent::Press(MouseButton::Left, x, y)) => {
+                if write!(screen, "{}x", Goto(x, y)).is_err() {
+                    break;
+                }
+            }
+
+            Event::Mouse(MouseEvent::Hold(x, y)) => {
+                if write!(screen, "{}x", Goto(x, y)).is_err() {
+                    break;
+                }
+            }
+
+            _ => {}
+        }
+
+        if write!(screen, "{}press q to exit", Goto(1, 1)).is_err() {
+            break;
+        }
+
+        if screen.flush().is_err() {
+            break;
+        }
+    }
+
+    if let Err(_) = write!(screen, "{}", cursor::Show) {
+        // well have to restart terminal to bring back cursor.
+    }
 }
